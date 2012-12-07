cd47fc22b0b5adcfe93a20b1ff79615b7e9eab51
M2tM <michael@tinyco.com>
2012-12-07 04:29:11 -0800

    Removing unused framework files, and removing apple switches in package.h files.

--- Source/Audio/package.h
+++ Source/Audio/package.h
@@ -1,8 +1,4 @@
 #ifndef __AUDIO_PACKAGE__
 #define __AUDIO_PACKAGE__
-#ifdef __APPLE__
-	#include "sound.h"
-#else
 	#include "Audio/sound.h"
-#endif
 #endif
--- Source/Physics/package.h
+++ Source/Physics/package.h
@@ -1,8 +1,4 @@
 #ifndef __PHYSICS_PACKAGE__
 #define __PHYSICS_PACKAGE__
-#ifdef __APPLE__
-	#include "collisionObjects.h"
-#else
 	#include "Physics/collisionObjects.h"
-#endif
 #endif

29530a41565ade01f1207ecf69e0354fa9744df4
M2tM <maxmike@gmail.com>
2016-09-05 18:35:32 -0700

    Main test stuff.
    Former-commit-id: 496eda11a8c231fc55c7f11988bac8d54de28d26 [formerly 87b80df4bf62d952ff82198308a27c03431bc13a]
    Former-commit-id: 14b5b9fefe54407763f9d2fb6fb8ecf29aa68857

--- Source/main.cpp
+++ Source/main.cpp
@@ -40,60 +40,63 @@ struct TestClassVersioned {
 
 #include "cereal/archives/json.hpp"
 
-int main(int, char *[]){
+int main(int, char *[]) {
 
-// 	pqxx::connection c("host=mutedvision.cqki4syebn0a.us-west-2.rds.amazonaws.com port=5432 dbname=bindstone user=m2tm password=Tinker123");
-// 	pqxx::work txn(c);
-// 
-// 
-// 	txn.exec(
-// 		"CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public;"
-// 		"CREATE TABLE Instances ("
-// 		"	Id SERIAL primary key,"
-// 		"	Available boolean			default false,"
-// 		"	Host text						default '',"
-// 		"	Port integer				default 0,"
-// 		"	PlayerLeft	int				default 0,"
-// 		"	PlayerRight	int				default 0,"
-// 		"	LastUpdate timestamp without time zone default (now() at time zone 'utc'),"
-// 		"	Result JSON"
-// 		");");
-// 	txn.commit();
+	// 	pqxx::connection c("host=mutedvision.cqki4syebn0a.us-west-2.rds.amazonaws.com port=5432 dbname=bindstone user=m2tm password=Tinker123");
+	// 	pqxx::work txn(c);
+	// 
+	// 
+	// 	txn.exec(
+	// 		"CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public;"
+	// 		"CREATE TABLE Instances ("
+	// 		"	Id SERIAL primary key,"
+	// 		"	Available boolean			default false,"
+	// 		"	Host text						default '',"
+	// 		"	Port integer				default 0,"
+	// 		"	PlayerLeft	int				default 0,"
+	// 		"	PlayerRight	int				default 0,"
+	// 		"	LastUpdate timestamp without time zone default (now() at time zone 'utc'),"
+	// 		"	Result JSON"
+	// 		");");
+	// 	txn.commit();
 
-// 	pqxx::result r = txn.exec(
-// 		"SELECT state "
-// 		"FROM players "
-// 		"WHERE email = " + txn.quote("maxmike@gmail.com"));
-// 
-// 	if (r.size() != 1)
-// 	{
-// 		std::cerr
-// 			<< "Expected 1 player with email " << txn.quote("maxmike@gmail.com") << ", "
-// 			<< "but found " << r.size() << std::endl;
-// 		return 1;
-// 	}
-// 
-// 	std::string status = r[0][0].c_str();
-// 	std::cout << "Updating employee #" << status << std::endl;
-
-
-	/*
-	pqxx::result r = txn.exec(
-		"select column_name, data_type, character_maximum_length"
-		"from INFORMATION_SCHEMA.COLUMNS where table_name = Players");
-
-	txn.commit();
+	// 	pqxx::result r = txn.exec(
+	// 		"SELECT state "
+	// 		"FROM players "
+	// 		"WHERE email = " + txn.quote("maxmike@gmail.com"));
+	// 
+	// 	if (r.size() != 1)
+	// 	{
+	// 		std::cerr
+	// 			<< "Expected 1 player with email " << txn.quote("maxmike@gmail.com") << ", "
+	// 			<< "but found " << r.size() << std::endl;
+	// 		return 1;
+	// 	}
+	// 
+	// 	std::string status = r[0][0].c_str();
+	// 	std::cout << "Updating employee #" << status << std::endl;
 
 
-	if (r.size() != 1)
-	{
-		std::cerr
-			<< "Expected 1 employee with name " << argv[1] << ", "
-			<< "but found " << r.size() << std::endl;
-		return 1;
+		/*
+		pqxx::result r = txn.exec(
+			"select column_name, data_type, character_maximum_length"
+			"from INFORMATION_SCHEMA.COLUMNS where table_name = Players");
+
+		txn.commit();
+
+
+		if (r.size() != 1)
+		{
+			std::cerr
+				<< "Expected 1 employee with name " << argv[1] << ", "
+				<< "but found " << r.size() << std::endl;
+			return 1;
+		}
+		*/
+
+	for (float i = 0.0f; i < 1.0f; i += .2f){
+		std::cout << i << ": " << MV::mixIn(-10.0f, 10.0f, i, 2.0f) << " => " << MV::unmixIn(-10.0f, 10.0f, i, 2.0f) << std::endl;
 	}
-	*/
-
 
 	//auto emailer = MV::Email::make("email-smtp.us-west-2.amazonaws.com", "587", { "AKIAIVINRAMKWEVUT6UQ", "AiUjj1lS/k3g9r0REJ1eCoy/xeYZgLXmB8Nrep36pUVw" });
 	//emailer->send({ "jai", "jackaldurante@gmail.com", "Derv", "maxmike@gmail.com" }, "Testing new Interface", "Does this work too?");

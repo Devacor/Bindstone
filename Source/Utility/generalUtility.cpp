20d7c3c62f7e419b780d8570d0c74da7567ab16d
Tobias Hi3C <tobias+hi3c@hieta.se>
2017-06-24 12:20:26 +0200

    Make generalUtility compile on Linux
    Don't include windows.h if we are not on Linux

--- Source/Utility/generalUtility.cpp
+++ Source/Utility/generalUtility.cpp
@@ -13,7 +13,9 @@
 	#include <sys/types.h>
 	#include <unistd.h>
 	#include <sys/sysctl.h>
-#else
+#endif
+
+#ifdef _WIN32
 	#include <windows.h>
 #endif
 
@@ -28,28 +30,28 @@ namespace MV {
 		int					  mib[4];
 		struct kinfo_proc	info;
 		size_t				  size;
-		
+
 		// Initialize the flags so that, if sysctl fails for some bizarre
 		// reason, we get a predictable result.
-		
+
 		info.kp_proc.p_flag = 0;
-		
+
 		// Initialize mib, which tells sysctl the info we want, in this case
 		// we're looking for information about a specific process ID.
-		
+
 		mib[0] = CTL_KERN;
 		mib[1] = KERN_PROC;
 		mib[2] = KERN_PROC_PID;
 		mib[3] = getpid();
-		
+
 		// Call sysctl.
-		
+
 		size = sizeof(info);
 		junk = sysctl(mib, sizeof(mib) / sizeof(*mib), &info, &size, NULL, 0);
 		assert(junk == 0);
-		
+
 		// We're being debugged if the P_TRACED flag is set.
-		
+
 		return ( (info.kp_proc.p_flag & P_TRACED) != 0 );
 	}
 #endif
@@ -69,7 +71,7 @@ namespace MV {
 				// error!
 			}
 			CFRelease(resourcesURL);
-			
+
 			chdir(path);
 			std::cout << "Current Path: " << path << std::endl;
 		}

385da0db320bfa42066755f5041ca0fecb99b11e
M2tM <maxmike@gmail.com>
2017-10-12 00:38:55 -0700

    Fixing chaiscript options for 6.0.0

--- Source/Utility/chaiscriptStdLib.h
+++ Source/Utility/chaiscriptStdLib.h
@@ -11,7 +11,6 @@ namespace chaiscript {
 
 //this separation of compilation module improves compile time.
 namespace MV {
-	std::shared_ptr<chaiscript::Module> create_chaiscript_stdlib();
 	std::vector<std::string> chaiscript_module_paths();
 	std::vector<std::string> chaiscript_use_paths();
 }

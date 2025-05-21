14262934a0940e059b8058b3d5e1fd4b820baf6c
Devacor <maxmike@gmail.com>
2025-05-21 01:13:04 -0500

    Working on properties.

--- Source/MV/Utility/properties.hpp
+++ Source/MV/Utility/properties.hpp
@@ -2,69 +2,261 @@
 #define _MV_PROPERTIES_H_
 
 #include <string>
+#include <map>
 #include <vector>
-#include "cereal/cereal.hpp"
+#include <functional>
+#include "require.hpp"
+#include "signal.hpp"
+#include <cereal/cereal.hpp>
+#include <cereal/types/string.hpp>
+#include <cereal/types/vector.hpp>
+#include <cereal/archives/json.hpp>
+#include <cereal/archives/binary.hpp>
 
 namespace MV {
 
-    class MvPropertyBase;
+	class PropertyBase;
 
-    class MvPropertyList {
-    public:
-        void add(MvPropertyBase* a_property) {
-            properties.push_back(a_property);
-        }
+	class PropertyRegistry {
+	public:
+		PropertyRegistry() = default;
+		PropertyRegistry(PropertyRegistry&&) = default;
 
-        const std::vector<MvPropertyBase*>& all() const { return properties; }
-    private:
-        std::vector<MvPropertyBase*> properties;
-    };
+		PropertyRegistry(const PropertyRegistry&) = delete;
+		PropertyRegistry& operator=(const PropertyRegistry&) = delete;
 
-    class PropertyHost {
-    public:
-        MvPropertyList Properties;
-    };
+		inline void add(PropertyBase* a_property);
 
-    class MvPropertyBase {
-    public:
-        MvPropertyBase() = default;
-        MvPropertyBase(MvPropertyList& a_list, const std::string& a_name) {
-            registerWith(a_list, a_name);
-        }
-        virtual ~MvPropertyBase() = default;
+		const std::map<std::string, PropertyBase*>& all() const { return properties; }
 
-        const std::string& name() const { return propertyName; }
-    protected:
-        void registerWith(MvPropertyList& a_list, const std::string& a_name) {
-            propertyName = a_name;
-            a_list.add(this);
-        }
-    private:
-        std::string propertyName;
-    };
+		template <class Archive>
+		void save(Archive& ar) const {
+			std::vector<std::string> keys;
+			for (const auto& [name, prop] : properties) {
+				keys.push_back(name);
+			}
+			ar(cereal::make_nvp("PropertyKeys", keys));
+			for (const auto& name : keys) {
+				properties.at(name)->save(ar);
+			}
+		}
 
-    template<typename T>
-    class Property : public MvPropertyBase {
-    public:
-        using value_type = T;
-        Property() = default;
-        Property(MvPropertyList& a_list, const std::string& a_name, const T& a_default = T{})
-            : MvPropertyBase(a_list, a_name), value(a_default) {}
+		template <class Archive>
+		void load(Archive& ar, const std::vector<std::string>& a_optionalKeyOrderOverride = {}) {
+			std::vector<std::string> savedKeys;
+			if (a_optionalKeyOrderOverride.empty())
+			{
+				ar(cereal::make_nvp("PropertyKeys", savedKeys));
+			} else {
+				savedKeys = a_optionalKeyOrderOverride;
+			}
 
-        Property& operator=(const T& a_val) { value = a_val; return *this; }
-        operator const T&() const { return value; }
-        const T& get() const { return value; }
-        T& get() { return value; }
+			std::map<std::string, bool> loaded;
+			for (const auto& name : savedKeys) {
+				auto it = properties.find(name);
+				MV::require<ResourceException>(it != properties.end(), "Unknown Property: ", name, " If a property was deleted, you will need to either recreate this archive, or, add a DeletedProperty<TheDeletedPropertyType> with this name.");
+				it->second->load(ar);
+				loaded[name] = true;
+			}
+		}
 
-        template<class Archive>
-        T save_minimal(const Archive&) const { return value; }
+		inline void cloneTo(PropertyRegistry& target) const;
 
-        template<class Archive>
-        void load_minimal(const Archive&, const T& a_val) { value = a_val; }
-    private:
-        T value{};
-    };
+	private:
+		std::map<std::string, PropertyBase*> properties;
+	};
 
-}
 
-#endif
+	class PropertyBase {
+	public:
+		virtual ~PropertyBase() = default;
+
+		bool operator<(const PropertyBase& rhs) const {
+			return name() < rhs.name();
+		}
+
+		const std::string& name() const { return propertyName; }
+
+		virtual void save(cereal::JSONOutputArchive& ar) const = 0;
+		virtual void load(cereal::JSONInputArchive& ar) = 0;
+		virtual void save(cereal::BinaryOutputArchive& ar) const = 0;
+		virtual void load(cereal::BinaryInputArchive& ar) = 0;
+
+		virtual void cloneInto(PropertyBase& target) = 0;
+
+	protected:
+		PropertyBase(const std::string& a_name)
+			: propertyName(a_name) {
+		}
+
+		PropertyBase(PropertyRegistry& a_list, const std::string& a_name)
+			: PropertyBase(a_name) {
+			a_list.add(this);
+		}
+
+	private:
+		std::string propertyName;
+	};
+
+	inline void PropertyRegistry::add(PropertyBase* a_property) {
+		properties[a_property->name()] = a_property;
+	}
+
+	inline void PropertyRegistry::cloneTo(PropertyRegistry& target) const {
+		for (const auto& [name, prop] : properties) {
+			auto it = target.properties.find(name);
+			if (it != target.properties.end()) {
+				prop->cloneInto(*it->second);
+			}
+		}
+	}
+
+	template<typename T>
+	class Property : public PropertyBase {
+	public:
+		using value_type = T;
+
+		Property(PropertyRegistry& a_registry, const std::string& a_name, const T& a_defaultVal = T{}, std::function<void(Property<T>&, Property<T>&)> a_customClone = {})
+			: PropertyBase(a_registry, a_name),
+			cloneIntoImplementation(std::move(a_customClone)),
+			value(a_defaultVal) {
+		}
+
+		Property(const Property<T>&) = delete;
+		Property& operator=(const Property<T>&) = delete;
+
+		Property& operator=(const T& a_val) {
+			value = a_val;
+			return *this;
+		}
+
+		operator const T& () const { return value; }
+
+		[[nodiscard]]
+		const T& get() const { return value; }
+
+		[[nodiscard]]
+		T& get() { return value; }
+
+		void save(cereal::JSONOutputArchive& ar) const override {
+			ar(cereal::make_nvp(name(), value));
+		}
+
+		void load(cereal::JSONInputArchive& ar) override {
+			ar(cereal::make_nvp(name(), value));
+		}
+
+		void save(cereal::BinaryOutputArchive& ar) const override {
+			ar(cereal::make_nvp(name(), value));
+		}
+
+		void load(cereal::BinaryInputArchive& ar) override {
+			ar(cereal::make_nvp(name(), value));
+		}
+
+		void cloneInto(PropertyBase& destination) override {
+			if(cloneIntoImplementation)
+			{
+				cloneIntoImplementation(static_cast<Property<T>&>(*this), static_cast<Property<T>&>(destination));
+			}else{
+				static_cast<Property<T>&>(destination).value = value;
+			}
+		}
+
+	protected:
+		T value{};
+		std::function<void(Property<T> &self, Property<T>& destination)> cloneIntoImplementation;
+	};
+
+	template<typename T>
+	bool operator==(const Property<T>& lhs, const Property<T>& rhs) {
+		return lhs.get() == rhs.get();
+	}
+
+	template<typename T>
+	bool operator==(const T& lhs, const Property<T>& rhs) {
+		return lhs == rhs.get();
+	}
+
+	template<typename T>
+	bool operator==(const Property<T>& lhs, const T& rhs) {
+		return lhs.get() == rhs;
+	}
+
+	template<typename T>
+	class DeletedProperty : public PropertyBase {
+	public:
+		DeletedProperty(PropertyRegistry& a_list, const std::string& a_name)
+			: PropertyBase(a_list, a_name) {
+		}
+
+		DeletedProperty(const std::string& a_name)
+			: PropertyBase(a_name) {
+		}
+
+		// No accessors, no value.
+		const T& get() const = delete;
+		T& get() = delete;
+
+		void save(cereal::JSONOutputArchive& ar) const override {}
+		void load(cereal::JSONInputArchive& ar) override {
+			T ignored;
+			ar(cereal::make_nvp(name(), ignored));
+		}
+
+		void save(cereal::BinaryOutputArchive& ar) const override {}
+		void load(cereal::BinaryInputArchive& ar) override {
+			T ignored;
+			ar(cereal::make_nvp(name(), ignored));
+		}
+
+		void cloneInto(PropertyBase&) override {
+			// No-op
+		}
+	};
+
+	template<typename T>
+	class ObservableProperty : public Property<T> {
+	public:
+		using ChangeSignature = void(const T& newValue, const T& oldValue, bool isFromLoad);
+
+		ObservableProperty(PropertyRegistry& registry, const std::string& name, const T& defaultValue = T{},
+			std::function<void(Property<T>&, Property<T>&)> cloneFn = {})
+			: Property<T>(registry, name, defaultValue, std::move(cloneFn)),
+			onChanged(onChangedSignal) {
+		}
+
+		// same as Property<T>::operator=(T), but also emits onChanged
+		ObservableProperty& operator=(const T& newVal) {
+			if (this->value != newVal) {
+				T oldVal = this->value;
+				this->value = newVal;
+				onChangedSignal(this->value, oldVal, false);
+			}
+			return *this;
+		}
+
+		void load(cereal::JSONInputArchive& ar) override {
+			T oldVal = this->value;
+			Property<T>::load(ar);
+			if (this->value != oldVal) {
+				onChangedSignal(this->value, oldVal, true);
+			}
+		}
+
+		void load(cereal::BinaryInputArchive& ar) override {
+			T oldVal = this->value;
+			Property<T>::load(ar);
+			if (this->value != oldVal) {
+				onChangedSignal(this->value, oldVal, true);
+			}
+		}
+
+		MV::SignalRegister<ChangeSignature> onChanged;
+	protected:
+		MV::Signal<ChangeSignature> onChangedSignal;
+	};
+
+} // namespace MV
+
+#endif // _MV_PROPERTIES_H_
